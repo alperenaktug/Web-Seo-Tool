81d4978e3fb142f6cd89b6732081692377179ec3
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-27 14:45:18 +0300

    error

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,22 @@ mongoose
   });
 
 const app = express();
+
 app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
 app.use("/backend/user", userRoutes);
-app.use("/backend/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
