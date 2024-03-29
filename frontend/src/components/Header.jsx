98e6394d7a7763d9b68244e6dd63d3b805f5008d
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-29 13:22:18 +0300

    header update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-sky-700">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-5">
@@ -16,8 +18,16 @@ export default function Header() {
           <Link to="/about ">
             <li className="text-lg rounded-2xl hover:scale-110 ">About</li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-lg rounded-2xl hover:scale-110 ">Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
