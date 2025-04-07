10da9e88e85d096fd0af8d4fa351ee0b11608458
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-04-08 00:57:47 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
@@ -19,10 +18,10 @@ mongoose
 const __dirname = path.resolve();
 
 const app = express();
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.use(express.json());
@@ -33,8 +32,8 @@ app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
-app.use("/backend/user", userRoutes);
-app.use("/backend/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
